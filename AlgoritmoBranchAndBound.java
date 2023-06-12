16602a0dc0e5f23fc7165a5d62f75daa1ee7eed8
Bruno Pietra <bfpietra@gmail.com>
2023-06-11 22:31:45 -0300

    codigo comentado

--- AlgoritmoBranchAndBound.java
+++ AlgoritmoBranchAndBound.java
@@ -17,14 +17,18 @@ public class AlgoritmoBranchAndBound {
     static List<Integer> lojasNãoAutorizadas = new ArrayList<>();
 
     public static void main(String[] args) {
-        long tempoInicial = System.currentTimeMillis();
+        long tempoInicial = System.currentTimeMillis(); 
 
         System.out.println("Branch and Bound!");
 
-        List<Loja> lojas = lerArquivo("lojas.txt");
+        List<Loja> lojas = lerArquivo("lojas.txt"); //le o arquivo e armazena seus dados em um array do tipo loja. Cada loja contem um id, suas coordenadas e itens para fornecer a outras lojas
 
-        calcularMelhorRota(new ArrayList<>(), 0, lojas);
-        setGastoGasolinaPorEstrada();
+        calcularMelhorRota(new ArrayList<>(), 0, lojas); //calcula a melhor rota com base no custo de gasolina
+
+        setGastoGasolinaPorEstrada(); //calcula o gasto de gasolina entre as lojas
+
+
+        //------------------------------------------------------inicio da exibicao de resultados no terminal------------------------------------------------------------
 
         System.out.print("\nMelhor rota: ");
         for (Loja loja : melhorRota){
@@ -43,42 +47,48 @@ public class AlgoritmoBranchAndBound {
 
         long tempoTotal = tempoFinal - tempoInicial;
 
-        System.out.println("Tempo de execução: " + tempoTotal + " milissegundos");
+        System.out.println("Tempo de execução: " + tempoTotal + " milissegundos"); //exibe o tempo de execucao do codigo
 
-        Grafico grafico = new Grafico(melhorRota, gastoGasolinaMelhorCaso, gastoGasolinaPorEstrada);
+        //------------------------------------------------------fim da exibicao de resultados no terminal--------------------------------------------------------------
+
+        Grafico grafico = new Grafico(melhorRota, gastoGasolinaMelhorCaso, gastoGasolinaPorEstrada); //exibe um grafico com base na melhor rota, seu custo total e o custo entre lojas
 
     }
 
     public static void calcularMelhorRota(List<Loja> rota, double gastoGasolina, List<Loja> lojas){
-        if (rota.size() == 0){
-            rota.add(lojas.get(0));
+        if (rota.size() == 0){ 
+            rota.add(lojas.get(0)); //A rota deve sempre começar na loja 0. Ao inserir uma loja na rota, nesse caso, é necessário atualizar apenas a carga do caminhao (não se atualiza o gasto de gasolina já que se começa com o caminhão na loja)
+            caminhao.atualizarCarga(lojas.get(0));
         }
 
         
     
         if (rota.size() == lojas.size()){
-            rota.add(lojas.get(0));
+            rota.add(lojas.get(0)); //A rota deve sempre terminar na loja 0. Ao inserir uma loja na rota, geralmente, é necessário atualizar a carga do caminhao e o gasto de gasolina
             double novoGastoGasolina = gastoGasolina + calcularGastoGasolinaAdicional(rota.get(rota.size() - 1), rota.get(rota.size() - 2));
             caminhao.atualizarCarga(lojas.get(0));
     
-            if (caminhao.getCargaAtual().size() == 0){
-                if (novoGastoGasolina < gastoGasolinaMelhorCaso){
+            if (caminhao.getCargaAtual().size() == 0){ //se a carga estiver vazia, isso significa que o caminhao entregou todos os itens
+                if (novoGastoGasolina < gastoGasolinaMelhorCaso){ //se o novo gasto da nova rota for menor que o antigo melhor gasto, este passará a ser o atual menor gasto e a melhor rota receberá o valor da rota recem calculada
                     gastoGasolinaMelhorCaso = novoGastoGasolina;
                     melhorRota = new ArrayList<>(rota);
                 }
             }
     
+            //Ao remover uma loja da rota, deve-se atualizar as variaveis relacionadas a ela, que são, nesse caso, a carga do caminhão
+
             rota.remove(rota.size() - 1);
             caminhao.setCargaAtual(new ArrayList<>());
         }
     
         for (int i = 1; i < lojas.size(); i++){
-            if (!rota.contains(lojas.get(i)) && !lojasNãoAutorizadas.contains(lojas.get(i).getId())){
+            if (!rota.contains(lojas.get(i)) && !lojasNãoAutorizadas.contains(lojas.get(i).getId())){ //criterio para evitar repeticoes e seguinte criterio de poda: se a loja for proibida de ser visitada, o ramo relacionado a ela é podado)
 
-                rota.add(lojas.get(i));
+                //Ao inserir uma loja na rota, deve-se atualizar as variaveis relacionadas a ela, que são, nesse caso, o gasto de gasolina, a carga do caminhão e a lista de lojas não autorizadas a serem visitadas
+
+                rota.add(lojas.get(i)); 
                 double novoGastoGasolina = gastoGasolina + calcularGastoGasolinaAdicional(rota.get(rota.size() - 1), rota.get(rota.size() - 2));
                 
-    
                 Caminhao cargaAnterior = new Caminhao();
                 cargaAnterior.setCargaAtual(new ArrayList<>(caminhao.getCargaAtual()));
 
@@ -89,23 +99,23 @@ public class AlgoritmoBranchAndBound {
                     lojasNãoAutorizadas.remove(elemento);
                 }
 
-    
-                if (caminhao.getCargaAtual().size() <= k && novoGastoGasolina < gastoGasolinaMelhorCaso){
-                    calcularMelhorRota(rota, novoGastoGasolina, lojas);
+                if (caminhao.getCargaAtual().size() <= k && novoGastoGasolina < gastoGasolinaMelhorCaso){ //poda caso a carga do caminhao tiver sido excedida e caso o gasto atual for maior que o gasto no melhor caso
+                    calcularMelhorRota(rota, novoGastoGasolina, lojas); 
                 }
+
+                //Ao remover uma loja da rota, deve-se atualizar as variaveis relacionadas a ela, que são, nesse caso, o gasto de gasolina, a carga do caminhão e a lista de lojas não autorizadas a serem visitadas
 
                 lojasNãoAutorizadas = new ArrayList<>(lojasNaoAutorizadasVersaoAnterior);
     
                 caminhao.setCargaAtual(new ArrayList<>(cargaAnterior.getCargaAtual()));
-                rota.remove(rota.size() - 1);
+                rota.remove(rota.size() - 1); 
             }
         }
     }
 
-    private static double calcularGastoGasolinaAdicional(Loja l1, Loja l2){
+    private static double calcularGastoGasolinaAdicional(Loja l1, Loja l2){ 
         double pitagoras = Math.sqrt(Math.pow(l1.getCoordenadaX() - l2.getCoordenadaX(), 2) + Math.pow(l1.getCoordenadaY() - l2.getCoordenadaY(), 2));
-
-        
+       
         double gastoGasolina = pitagoras / (10 - (0.5 * caminhao.getCargaAtual().size())); 
 
         return gastoGasolina;
@@ -138,7 +148,7 @@ public class AlgoritmoBranchAndBound {
                 
                 for (int i = 3; i < partes.length; i++) {
                     lojasReceber.add(Integer.parseInt(partes[i]));
-                    lojasNãoAutorizadas.add(Integer.parseInt(partes[i]));
+                    lojasNãoAutorizadas.add(Integer.parseInt(partes[i])); 
                 }
                 
                 Loja loja = new Loja(id, coordenadaX, coordenadaY, lojasReceber);
--- AlgoritmoForcaBruta.java
+++ AlgoritmoForcaBruta.java
@@ -20,12 +20,17 @@ public class AlgoritmoForcaBruta {
 
         System.out.println("Forca Bruta!");
 
-        List<Loja> lojas = lerArquivo("lojas.txt");
+        List<Loja> lojas = lerArquivo("lojas.txt"); //le o arquivo e armazena seus dados em um array do tipo loja. Cada loja contem um id, suas coordenadas e itens para fornecer a outras lojas
         
-        boolean estourouCarga = false;
-        calcularMelhorRota(new ArrayList<>(), 0, lojas, estourouCarga);
+        boolean estourouCarga = false; //variavel que serve para informar se a carga do caminhao foi excedida
 
-        setGastoGasolinaPorEstrada();
+        calcularMelhorRota(new ArrayList<>(), 0, lojas, estourouCarga); //calcula a melhor rota com base no custo de gasolina
+
+        setGastoGasolinaPorEstrada(); //calcula o gasto de gasolina entre as lojas
+
+
+        //------------------------------------------------------inicio da exibicao de resultados no terminal------------------------------------------------------------
+
 
         System.out.print("\nMelhor rota: ");
         for (Loja loja : melhorRota){
@@ -44,50 +49,61 @@ public class AlgoritmoForcaBruta {
 
         long tempoTotal = tempoFinal - tempoInicial;
 
-        System.out.println("Tempo de execução: " + tempoTotal + " milissegundos");
+        System.out.println("Tempo de execução: " + tempoTotal + " milissegundos"); //exibe o tempo de execucao do codigo
 
-        Grafico grafico = new Grafico(melhorRota, gastoGasolinaMelhorCaso, gastoGasolinaPorEstrada);
+
+
+        //------------------------------------------------------fim da exibicao de resultados no terminal--------------------------------------------------------------
+
+
+        Grafico grafico = new Grafico(melhorRota, gastoGasolinaMelhorCaso, gastoGasolinaPorEstrada); //exibe um grafico com base na melhor rota, seu custo total e o custo entre lojas
 
     }
 
     public static void calcularMelhorRota(List<Loja> rota, double gastoGasolina, List<Loja> lojas, boolean estourouCarga){
         if (rota.size() == 0){
-            rota.add(lojas.get(0));
+            rota.add(lojas.get(0)); //A rota deve sempre começar na loja 0. Ao inserir uma loja na rota, nesse caso, é necessário atualizar apenas a carga do caminhao (não se atualiza o gasto de gasolina já que se começa com o caminhão na loja)
+            caminhao.atualizarCarga(lojas.get(0));
         }
     
         if (rota.size() == lojas.size()){
-            rota.add(lojas.get(0));
+            rota.add(lojas.get(0)); //A rota deve sempre terminar na loja 0. Ao inserir uma loja na rota, geralmente, é necessário atualizar a carga do caminhao e o gasto de gasolina
             double novoGastoGasolina = gastoGasolina + calcularGastoGasolinaAdicional(rota.get(rota.size() - 1), rota.get(rota.size() - 2));
             caminhao.atualizarCarga(lojas.get(0));
     
-            if (caminhao.getCargaAtual().size() == 0 && !estourouCarga){
-                if (novoGastoGasolina < gastoGasolinaMelhorCaso){
+            if (caminhao.getCargaAtual().size() == 0 && !estourouCarga){ //se a carga estiver vazia, isso significa que o caminhao entregou todos os itens e, se a carga do caminhao não estourou o limite, significa que a rota foi valida
+                if (novoGastoGasolina < gastoGasolinaMelhorCaso){ //se o novo gasto da nova rota for menor que o antigo melhor gasto, este passará a ser o atual menor gasto e a melhor rota receberá o valor da rota recem calculada
                     gastoGasolinaMelhorCaso = novoGastoGasolina;
                     melhorRota = new ArrayList<>(rota);
                 }
             }
     
-            rota.remove(rota.size() - 1);
+            //Ao remover uma loja da rota, deve-se atualizar as variaveis relacionadas a ela, que são, nesse caso, a carga do caminhão
+
+            rota.remove(rota.size() - 1); 
             caminhao.setCargaAtual(new ArrayList<>());
         }
     
         for (int i = 1; i < lojas.size(); i++){
-            if (!rota.contains(lojas.get(i))){
+            if (!rota.contains(lojas.get(i))){ //criterio para evitar repeticoes
+
+                //Ao inserir uma loja na rota, deve-se atualizar as variaveis relacionadas a ela, que são, nesse caso, o gasto de gasolina e a carga do caminhão
+
                 rota.add(lojas.get(i));
                 double novoGastoGasolina = gastoGasolina + calcularGastoGasolinaAdicional(rota.get(rota.size() - 1), rota.get(rota.size() - 2));
                 
-    
                 Caminhao caminhaoVersaoAnterior = new Caminhao();
                 caminhaoVersaoAnterior.setCargaAtual(new ArrayList<>(caminhao.getCargaAtual()));
     
                 caminhao.atualizarCarga(lojas.get(i));
     
-                if (caminhao.getCargaAtual().size() > k){
+                if (caminhao.getCargaAtual().size() > k){ //a carga do caminhao tera estourado caso ela ultrapasse o valor da variável k
                     estourouCarga = true;
                 }
     
                 calcularMelhorRota(rota, novoGastoGasolina, lojas, estourouCarga);
                 
+                //Ao remover uma loja da rota, deve-se atualizar as variaveis relacionadas a ela, que são, nesse caso, o gasto de gasolina e a carga do caminhão
     
                 caminhao.setCargaAtual(new ArrayList<>(caminhaoVersaoAnterior.getCargaAtual()));
                 rota.remove(rota.size() - 1);
@@ -99,7 +115,6 @@ public class AlgoritmoForcaBruta {
     private static double calcularGastoGasolinaAdicional(Loja l1, Loja l2){
         double pitagoras = Math.sqrt(Math.pow(l1.getCoordenadaX() - l2.getCoordenadaX(), 2) + Math.pow(l1.getCoordenadaY() - l2.getCoordenadaY(), 2));
 
-        
         double gastoGasolina = pitagoras / (10 - (0.5 * caminhao.getCargaAtual().size())); 
 
         return gastoGasolina;
